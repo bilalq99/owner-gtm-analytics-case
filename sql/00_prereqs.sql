1f2797f517e5cddfb55e4ec7053158d9b997e98f
bilalq99 <133537558+bilalq99@users.noreply.github.com>
2026-02-09 15:28:42 -0600

    Create 00_prereqs.sql

---
+++ sql/00_prereqs.sql
@@ -0,0 +1,6 @@
+use database DEMO_DB;
+use schema DE_CASE_BILALQURESHI0309_SCHEMA;
+
+-- Convenience window for H1’24 views
+set h1_start = to_date('2024-01-01');
+set h1_end   = to_date('2024-06-30');
